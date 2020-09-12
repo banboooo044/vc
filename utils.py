f15c0be97845e4e8c2a2d3324d7e4157425df1b6
banboooo044 <banboooo044@banboooo044noMacBook-Pro.local>
2020-09-12 15:47:47 +0900

    amen

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ import torch.nn.init as init
 
 def cc_data(data):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    data = data.to(data)
+    data = data.to(device)
 
 def cc_model(net):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
