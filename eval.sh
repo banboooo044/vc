1bfe6b193bb5599c2150de3d417ec8ac05ea594e
banboooo044 <banboooo044@banboooo044noMacBook-Pro.local>
2020-09-12 21:30:52 +0900

    eval sh

---
+++ eval.sh
@@ -0,0 +1,8 @@
+python3 eval.py -c config.yaml \
+-d /home/data/mel_data \
+-test_set in_test \
+-test_index_file in_test_samples_128.json \
+-test_label_file in_test-label.pkl \
+-load_model_path /home/vc/model \
+-attr /home/vc/model/exp2.args.yaml \
+-t exp2
