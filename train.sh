19c288bfcc58eb772a9123f2bdfb8335c56d777d
banboooo044 <banboooo044@banboooo044noMacBook-Pro.local>
2020-09-13 10:10:09 +0900

    annealing iter

--- train.sh
+++ train.sh
@@ -6,6 +6,9 @@ python3 main.py \
 --use_eval_set \
 -eval_set in_test \
 -eval_index_file in_test_samples_128.json \
+--use_test_set \
+-test_set out_test \
+-test_index_file out_test_samples_128.json \
 -logdir /home/vc/log \
 -store_model_path /home/vc/model \
 -tag exp3 \
